368287ad0d28c468b16d2594b2f8ba901bea8552
Vitalii Ivanchenko <vanno1234567890@gmail.com>
2025-04-29 21:46:53 +0300

    Add base layout and index view with templates

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'restaurant_kitchen_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'kitchen/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
