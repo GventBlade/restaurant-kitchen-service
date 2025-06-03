d6976acfd60861f5242db6801a054f6122cefff5
Vitalii Ivanchenko <vanno1234567890@gmail.com>
2025-06-03 14:51:02 +0300

    update settings

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -29,9 +29,9 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
